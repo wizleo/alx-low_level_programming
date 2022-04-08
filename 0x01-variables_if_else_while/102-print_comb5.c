35e5fd8f75386b2ed37fe4e1dd09b382b22faa79
wizleo <nkajjasolomon420@gmail.com>
2022-04-08 23:11:12 +0000

    12 commit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -19,7 +19,7 @@ putchar((num1 / 10) + '0');
 putchar((num1 % 10) + '0');
 putchar(' ');
 putchar((num2 / 10) + '0');
-putchar((num2 / 10) + '0');
+putchar((num2 % 10) + '0');
 
 if (num1 == 98 && num2 == 99)
 continue;
