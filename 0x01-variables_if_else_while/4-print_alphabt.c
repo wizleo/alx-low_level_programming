c715131865ebc2d30ed4b92e89ade54cbffdf179
wizleo <nkajjasolomon420@gmail.com>
2022-04-08 20:27:04 +0000

    4 commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: prints the lower case alphabet excluding e and q
+ * Return: 0
+ */
+int main(void)
+{char c;
+
+for (c = 'a'; c < = 'z'; ++c)
+{
+if (c != 'e' && c != 'q')
+putchar(c);
+}
+
+putchar('\n');
+
+return (0);
+}
