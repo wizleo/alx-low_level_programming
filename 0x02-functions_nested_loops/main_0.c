edfdeb7cd2468485fba75576accf169730eb7763
wizleo <nkajjasolomon420@gmail.com>
2022-04-13 04:41:06 +0000

    last commit

---
+++ 0x02-functions_nested_loops/main_0.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ *  * main - check the code for Holberton School students.
+ *   *
+ *    * Return: Always 0.
+ */
+int main(void)
+{
+		print_times_table;
+			return (0);
+}
