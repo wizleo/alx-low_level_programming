5b4aa87dfab1a5b85ffb8a5b3c02aa40e961dc04
wizleo <nkajjasolomon420@gmail.com>
2022-04-14 16:53:21 +0000

    task1 commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - mltiplies two integers
+ * @a: int is to be multiplied by b
+ * @b: int is to be multiplied by a
+ * Return result of the operation
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+
+	return (c);
+}
