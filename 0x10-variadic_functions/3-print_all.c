3f1517e19fbe173fae6a3c8bd741c18b43541226
wizleo <nkajjasolomon420@gmail.com>
2022-05-12 22:45:50 +0000

    0x10-variadic_functions corrections commit

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -89,8 +89,8 @@ void print_all(const char * const format, ...)
 	printer_t funcs[] = {
 		{"c", print_char},
 		{"i", print_int},
-		{"i", print_int},
-		{"i", print_int},
+		{"f", print_float},
+		{"s", print_string}
 	};
 
 	va_start(args, format);
