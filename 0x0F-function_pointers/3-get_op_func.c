4dbcd9a897cf365760dc17fba4f3300652a461f3
wizleo <nkajjasolomon420@gmail.com>
2022-05-12 02:18:15 +0000

    pointers corrections

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -5,11 +5,11 @@
 #include "3-calc.h"
 
 /**
- *  * get_op_func -  selects the correct function to
- *   * perform the operation asked by the user
- *    * @s: input string
- *     * Return: pointer to a function
- *     */
+ *get_op_func -  selects the correct function to
+ *perform the operation asked by the user
+ *@s: input string
+ *Return: pointer to a function
+ */
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
