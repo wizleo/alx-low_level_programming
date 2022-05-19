07522c25b2755e4c58f84da270af3e6b7d722e24
wizleo <nkajjasolomon420@gmail.com>
2022-05-19 22:43:58 +0000

    corrction

--- 0x12-singly_linked_lists/0-main.c
+++ 0x12-singly_linked_lists/0-main.c
@@ -6,13 +6,11 @@
  *main - check the code for Holberton School students.
  *
  *Return: Always 0.
- */ int main(void)
-{
+ */ int main(void){
 	list_t *head;
 	list_t *new;
 	list_t hello = {"World", 5, NULL};
 	size_t n;
-
 	head = &hello;
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
